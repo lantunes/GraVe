b834b864444f38687f3fcb9e434eedcda3ad5ff9
Luis Antunes <lantunes@gmail.com>
2020-10-02 06:39:19 -0400

    adding setup.py

--- scripts/README.md
+++ scripts/README.md
@@ -4,4 +4,4 @@
 The Cora dataset associates a binary feature vector with each node. This feature vector has 1,433 components, each 
 indicating the presence or absence of a particular word in the paper represented by the node. LDA was used to reduce 
 the number of components in the feature vector to 3, as each node is also assigned 1 of 7 labels. The 3D scatter plots 
-above depict the results (the same plot is shown from two different angles).    
+above depict the results (the same plot is shown from two different angles). The colors represent the different labels.    
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name="GraVe",
+      version="0.1.0",
+      description="GraVe: Graph Vectors.",
+      long_description="Graph vectors.",
+      license="Apache License 2.0",
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+      ],
+      url='http://github.com/lantunes/GraVe',
+      author="Luis M. Antunes",
+      author_email="lantunes@gmail.com",
+      packages=["grave"],
+      keywords=["graphs", "embeddings"],
+      python_requires='>3.5.6',
+      install_requires=["numpy >= 1.15.2", "autograd >= 1.3", "networkx >= 2.4", "scipy >= 1.1.0"])
