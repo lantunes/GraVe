428dac09a3b0645fd3a279a688088223bf971acb
Luis Antunes <lantunes@uncharted.software>
2020-10-03 19:07:45 -0400

    adding a "where greater" feature combiner

--- grave/feature_combiners.py
+++ grave/feature_combiners.py
@@ -20,3 +20,15 @@ def bitwise_or_feature_combiner(fv_word1, fv_word2):
     :return: a single feature vector representing word1 and word2
     """
     return np.bitwise_or(fv_word1, fv_word2)
+
+
+def where_greater_feature_combiner(fv_word1, fv_word2):
+    """
+    Returns a vector containing the greatest of the two values between fv_word1 and fv_word2 at each position.
+    :param fv_word1: the feature vector for the first word of the co-occurrence pair
+    :param fv_word2: the feature vector for the second word of the co-occurrence pair
+    :return: a single feature vector representing word1 and word2
+    """
+    a = np.array(fv_word1, copy=False)
+    b = np.array(fv_word2, copy=False)
+    return np.where(a > b, a, b)
